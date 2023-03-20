64986b151ef2bbcfdb01b3c3fe593c0bf9cd10f3
abdullah <a.ylcin47@gmail.com>
2023-03-20 15:33:49 +0300

    mongo db bağlantı testi yapıldı şuan bağlantı sorun var düzeltilip tekar girilecek

--- server.js
+++ server.js
@@ -36,7 +36,31 @@
 const express = require('express');
 const http = require('http');
 const socket = require('socket.io');
+// const mongodb = require('mongodb');
+// const dbUrl ='mongodb://localhost:27017';
 
+// const client = mongodb.MongoClient;
+
+
+
+// client.connect('mongodb://localhost:27017',(err,db)=>{
+//     console.log('err',err)
+//     console.log('db',db)
+//     const mydb = db.db('vkod',{Logger:(L) =>{console.log('1',1);}})
+//     mydb.createCollection('authors').then((r) => {
+//         console.log('r',r);
+//     }).catch((err) => {
+//         console.log("hata oluştu")
+//     });
+// })
+
+// client.connect(dbUrl,(err,db)=>{
+//     if (db) {
+//         console.log("bağlantı başarılı");
+//     }else{
+//         console.log("veri tabanı bağlantı hatası var")
+//     }
+// })
 const app = express();
 const server = http.createServer(app);
 
@@ -59,3 +83,9 @@ io.on('connection',(socket)=>{
         socket.broadcast.emit('typing' ,data)
     })
 });
+
+
+app.get('/api' ,(req,res)=>{ 
+res.send("sayfaya hoşgeldiniz")
+
+})
